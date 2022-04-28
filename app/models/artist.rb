c7762148e428e3ef72df7a42d5898157a7a59d9f
Andrey Koleshko <ka8725@gmail.com>
2022-04-28 02:09:54 +0200

    define enum

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,3 @@
 class Artist < ApplicationRecord
-  has_many :songs, -> { Thread.current[:current_user] ? all : only_public }
+  has_many :songs, -> { Thread.current[:current_user] ? all : where(status: :published) }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :artist
 
+  enum :status, {draft: 'draft', published: 'published'}
   scope :only_public, -> { where(status: 'published') }
 end
